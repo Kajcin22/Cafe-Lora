4be3b3ae9d8d626844c97a42be40ef67323e0ee2
Kajcin22 <masekkaterina@gmail.com>
2022-04-30 11:14:31 +0200

    při kliknutí skrývat menu

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import './style.css';
 
 console.log('funguju!');
 const navBtn = document.querySelector('#nav-btn');
+const nav = document.querySelector('nav');
 
 navBtn.addEventListener('click', (e) => {
-  document.querySelector('nav').classList.toggle('nav-closed');
+  nav.classList.toggle('nav-closed');
+  //nav.querySelectorAll('a').add('nav-closed');
 });
+
+const hideNav = () => {
+  nav.classList.add('nav-closed');
+};
+const navItems = nav.querySelectorAll('a');
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener('click', hideNav);
+}
