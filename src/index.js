2f328c67183f45275c444c8396e9d3f2c3beb34c
Kajcin22 <masekkaterina@gmail.com>
2022-04-30 11:49:00 +0200

    zprovozněno tlačítko pro objednávání

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import './style.css';
 
 console.log('funguju!');
+
+//rozbalování hamburger menu:
 const navBtn = document.querySelector('#nav-btn');
 const nav = document.querySelector('nav');
 
 navBtn.addEventListener('click', (e) => {
   nav.classList.toggle('nav-closed');
-  //nav.querySelectorAll('a').add('nav-closed');
 });
 
+//skrývání menu po kliknutí:
 const hideNav = () => {
   nav.classList.add('nav-closed');
 };
@@ -16,3 +18,23 @@ const navItems = nav.querySelectorAll('a');
 for (let i = 0; i < navItems.length; i++) {
   navItems[i].addEventListener('click', hideNav);
 }
+
+//objednávání:
+
+const orderBtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+let ordered = false;
+
+orderBtn.addEventListener('click', (e) => {
+  let btn = e.target;
+
+  if (ordered) {
+    btn.textContent = 'Objednat';
+    drinkCup.classList.remove('drink__cup', 'drink__cup--selected');
+    ordered = false;
+  } else {
+    btn.textContent = 'Zrušit';
+    drinkCup.classList.add('drink__cup', 'drink__cup--selected');
+    ordered = true;
+  }
+});
