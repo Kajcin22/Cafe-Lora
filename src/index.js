779b381082e18b3e855d3e785610368b68d8d925
Kajcin22 <masekkaterina@gmail.com>
2022-04-30 10:30:46 +0200

    zprovoznění hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css';
 
 console.log('funguju!');
+const navBtn = document.querySelector('#nav-btn');
+
+navBtn.addEventListener('click', (e) => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
